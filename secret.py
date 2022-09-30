1a410ad17b8c0a247eee1cef4c85f4d37ddfd4df
XuantongMa <xuantong@ualberta.ca>
2022-09-30 16:07:02 -0600

    lab

--- secret.py
+++ secret.py
@@ -12,8 +12,8 @@ class FollowingTheTAsInstructionsError(Exception):
         ))
 
 # Delete this line:
-raise FollowingTheTAsInstructionsError
+#raise FollowingTheTAsInstructionsError
 
-# Edit the following two lines:
-username = "<pick a username here>"
-password = "<pick a password here>"
+#Edit the following two lines:
+username = "cmput404"
+password = "123456"
